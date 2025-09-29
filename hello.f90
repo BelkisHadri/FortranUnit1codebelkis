1478f09dda4b25d20b9010db8ed35bc10548e129
BelkisHadri <belkis.hadri@thekaustschool.org>
2025-09-29 13:03:22 +0300

    Add files via upload

--- hello.f90
+++ hello.f90
@@ -4,33 +4,26 @@ implicit none
     character(len=20)::name
     integer::day
      integer :: temp, humidity, aqi
-    character(len=20) :: health
+    character(len=20) :: health_comment
     Print *, "Welcome to your Weather Quality checker, Where I will help you understand the circumstances of Thuwal weather to"// &
         "know if it's healthy or not!"
     Print *,"What is your name?"
     read *,name
     Print *,"Welcome ", name,"! can't wait to help you ! Please enter the date you want the weather on. (P.S, it should be "// &
          "from this month(October)!):"
-
-    do
+ 
+ do
         read *, day
 
         if (day >= 1 .and. day <= 31) then
             print *, "Thank you for inserting a correct date! You chose the day October", day
-            print *, "Let's start with your calculation!"
-            exit   
+            print *, "Now here is the data for that day:"
+            if (day==1) then 
+            temp=36
+            humidity=
         else
             print *, "Invalid day of the month. Please try again (1-31)."
         end if
-       if (day == 1) then
-        temp = 36
-        humidity = 55
-        aqi = 95
-        health = "Moderate"
-        end if
     end do
-
-
- 
 
 end program hello
